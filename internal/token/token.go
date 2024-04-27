008b245b9a1deb5f3e660c3895270008a42a6836
Aden-Q <qian0102@umn.edu>
2024-04-26 21:24:43 -0500

    feat: add token and lexer

--- internal/token/token.go
+++ internal/token/token.go
@@ -32,3 +32,10 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+func New(tokenType TokenType, literal string) *Token {
+	return &Token{
+		Type:    tokenType,
+		Literal: literal,
+	}
+}
