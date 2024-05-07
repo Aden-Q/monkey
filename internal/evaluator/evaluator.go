c2935577d70b49fde52059aea161dc9b725afcdf
Aden-Q <qian0102@umn.edu>
2024-05-07 18:08:20 -0500

    feat: initial test and implementation of evaluator

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -1,1 +1,52 @@
 package evaluator
+
+import (
+	"github.com/aden-q/monkey/internal/ast"
+	"github.com/aden-q/monkey/internal/object"
+)
+
+// interface compliance check
+var _ Evaluator = (*evaluator)(nil)
+
+// An interpreter/evaluator interface
+type Evaluator interface {
+	Eval(node ast.Node) (object.Object, error)
+}
+
+type evaluator struct {
+}
+
+func New() Evaluator {
+	return &evaluator{}
+}
+
+// Evaluate recursively evaluate an AST node
+func (e *evaluator) Eval(node ast.Node) (object.Object, error) {
+	switch node.(type) {
+	case *ast.Program:
+		return e.evalStatements(node.(*ast.Program).Statements)
+	case *ast.ExpressionStatement:
+		return e.Eval(node.(*ast.ExpressionStatement).Expression)
+	case *ast.IntegerExpression:
+		return &object.Integer{Value: node.(*ast.IntegerExpression).Value}, nil
+	case *ast.BooleanExpression:
+		return &object.Boolean{Value: node.(*ast.BooleanExpression).Value}, nil
+	}
+
+	return nil, nil
+}
+
+func (e *evaluator) evalStatements(stmts []ast.Statement) (object.Object, error) {
+	var result object.Object
+	var err error
+
+	// iteratively evaluate each statement and return the result from the last one
+	for _, stmt := range stmts {
+		result, err = e.Eval(stmt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
+}
--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -1,10 +1,49 @@
 package evaluator_test
 
 import (
+	"github.com/aden-q/monkey/internal/ast"
+	"github.com/aden-q/monkey/internal/evaluator"
+	"github.com/aden-q/monkey/internal/lexer"
+	"github.com/aden-q/monkey/internal/object"
+	"github.com/aden-q/monkey/internal/parser"
 	. "github.com/onsi/ginkgo/v2"
-	_ "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Evaluator", func() {
+	var (
+		text    string
+		l       lexer.Lexer
+		p       parser.Parser
+		e       evaluator.Evaluator
+		program *ast.Program
+		errs    []error
+	)
 
+	BeforeEach(func() {
+		l = lexer.New()
+		p = parser.New(l)
+		e = evaluator.New()
+	})
+
+	Describe("Eval", func() {
+		Context("integer object", func() {
+			It("integer expressions", func() {
+				text = `
+				5;
+				`
+				expectedObject := object.NewInteger(5)
+				expectedErrors := []error{}
+
+				// parse the program
+				program, errs = p.ParseProgram(text)
+				Expect(errs).To(Equal(expectedErrors))
+
+				// evaluate the AST tree
+				obj, err := e.Eval(program)
+				Expect(err).To(BeNil())
+				Expect(obj).To(Equal(expectedObject))
+			})
+		})
+	})
 })
